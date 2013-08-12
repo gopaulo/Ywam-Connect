399911829edca6b9a68cffe682a84b03cd0ee907
Paulo Vieira <paulo.vieira@agenciavivavida.com.br>
2013-08-12 18:28:19 -0300

    base template - push

---
+++ themes/ywamconnect/single-base.php
@@ -0,0 +1,23 @@
+<?php
+
+/* Template Name: Home */
+
+get_header();
+?>
+
+
+<div class="container">
+<div class="col-lg-3" > 
+	<?php get_sidebar('base'); ?>
+ </div>
+<div class="col-lg-6"> 
+	<div class="container">
+		<?php get_template_part('templates/map/map');?>
+	</div> 
+</div>
+<div class="col-lg-3" > 
+	 <?php get_sidebar('newsfeed');?>
+	</div>
+</div>
+
+<?php get_footer(); ?>
