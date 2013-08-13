5b883555b19eb61aca889d2ca466f1982727b16f
Paulo Vieira <paulo.vieira@agenciavivavida.com.br>
2013-08-13 19:39:52 -0300

    modal width styling

--- themes/ywamconnect/templates/modals/events.php
+++ themes/ywamconnect/templates/modals/events.php
@@ -1,4 +1,4 @@
-  <!-- Modal -->
+  <!-- Add Event Modal -->
   <div class="modal fade" id="addeventmodal">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -16,3 +16,22 @@
       </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
   </div><!-- /.modal -->
+
+   <!-- View Event Modal -->
+  <div class="modal fade" id="vieweventmodal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title">@Event Name</h4>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
+
