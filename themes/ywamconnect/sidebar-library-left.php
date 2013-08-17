b98de096480d7cd904338c04fff904c190b584fc
Paulo Vieira <paulo.vieira@agenciavivavida.com.br>
2013-08-17 00:02:06 -0300

    start of library wireframe

---
+++ themes/ywamconnect/sidebar-library-left.php
@@ -0,0 +1,34 @@
+<h4> Search Library </h4>
+<form id="searchlibraryform">
+	<div class="input-group search-courses">
+	 <input type="text" class="coursesearch" placeholder="Search Library">
+		<span class="input-group-addon"><i class="icon-search"></i></span>
+	</div>
+
+	<div class="divider"> </div>
+
+	<h4> Search Filters </h4>
+
+	<h6 class="search-filters"> Topic </h6>
+	<hr class="nomargin"/>
+	<ul id="course-search-filters">
+	  <li> <span class="searchtag green"></span><span class="checkbox-filter"><input type="checkbox" name="dts" id="check-dts" class="filtercheck" data-tag="dts" /> </span><label for="check-dts" class="filtertag-name"> DTS & Outreach</label></li>
+	  <li> <span class="searchtag red"></span><span class="checkbox-filter"><input type="checkbox" name="schools" id="check-schools" class="filtercheck" data-tag="schools" /> </span><label  for="check-schools" class="filtertag-name"> Schools</label></li>
+	  <li> <span class="searchtag orange"></span><span class="checkbox-filter"><input type="checkbox" name="events" id="check-events" class="filtercheck" data-tag="events" /> </span><label  for="check-events" class="filtertag-name"> Events</label></li>
+	  <li> <span class="searchtag yellow"></span><span class="checkbox-filter"><input type="checkbox" name="meetings" id="check-meetings" class="filtercheck" data-tag="meetings" /> </span><label  for="check-meetings" class="filtertag-name"> Meetings</label></li>
+	  <li> <span class="searchtag petroleum"></span><span class="checkbox-filter"><input type="checkbox" name="interviews" id="check-interviews" class="filtercheck" data-tag="interviews" /> </span><label  for="check-interviews" class="filtertag-name"> Interviews</label></li>
+	  <li> <span class="searchtag purple"></span><span class="checkbox-filter"><input type="checkbox" name="conferences" id="check-conferences" class="filtercheck" data-tag="conferences" /> </span><label  for="check-conferences" class="filtertag-name"> Conferences</label></li>
+	  <li> <span class="searchtag gray"></span><span class="checkbox-filter"><input type="checkbox" name="events" id="check-events" class="filtercheck" data-tag="events" /> </span><label  for="check-events" class="filtertag-name"> Events</label></li>
+
+	</ul>
+	<input type="hidden" name="year" value="2013">
+	<h6 class="search-filters"> Authors <small class="libraryHead-filter"> <a href="#" class="chosenType" data-type="first_name"> First</a> / <a href="#" data-type="last_name"> Last</a> name</small> </h6>
+	<hr class="nomargin"/>
+	<ul id="course-search-filters">
+
+	</ul>
+
+	<div class="divider"> </div>
+
+	<h4 class="course-results-title"> Search Results </h4><hr class="nomargin"/>
+</form>
---
+++ themes/ywamconnect/sidebar-library-right.php
@@ -0,0 +1,11 @@
+<h4> Most Viewed Videos </h4>
+<div class="librarysideframe">
+</div>
+
+<h4> Recently Posted Videos </h4>
+<div class="librarysideframe">
+</div>
+<div class="divider"></div>
+<p class="quote">
+Online  content  gives  every YWAMer access to  continuing  education,  cultural  enrichment and  the  knowledge  of  God's  word. You can now  hear  teachings  from  Christian  thought leaders,  listen  to  inspiring  testimonies  and participate  in  online  events  via  internet access  in  the  convenience  of  your home..
+</p>
